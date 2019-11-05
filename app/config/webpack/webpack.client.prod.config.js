2aa117be30b480f97a68f398b4c74011d7702472
Juan Contreras <juan.contreras@intercorpretail.pe>
2019-11-05 10:10:19 -0500

    build(config): se agrega minificado en webpack prod

--- app/config/webpack/webpack.client.prod.config.js
+++ app/config/webpack/webpack.client.prod.config.js
@@ -52,7 +52,15 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './public/favicon.ico'
+      favicon: './public/favicon.ico',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
     }),
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
